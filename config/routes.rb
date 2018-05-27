fbba371ecf74636eaac599f11be747e0a87a4de2
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2018-05-27 15:20:36 +0200

    Add shift routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post "user_token" => "user_token#create"
 
   resources :users, except: [:show]
+  resources :shifts
+
   get "users/current" => "users#current"
 end
